a821686583e80e2e1e72f8a4b82f8783280c8b96
samuel_tuki <smauel_tuki@yahoo.com>
2023-06-03 14:56:29 +0100

    eventHandler js function

--- assets/page.js
+++ assets/page.js
@@ -1,3 +1,9 @@
-function  hoverIngredient{
-    document.getElementById("ingredient").firstElementChild.firstElementChild.style.fontSize = "300%";
+function hoverIngredient() {
+  document.getElementById("ingredients").firstElementChild.style.fontSize =
+    "300%";
+}
+
+function normalIngredient() {
+  document.getElementById("ingredients").firstElementChild.style.fontSize =
+    "100%";
 }
